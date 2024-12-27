cb5d42e6c82c1b735a0f86ff8b5a84df345019ee
Liban Tahsin <tahsin.liban0927@gmail.com>
2024-12-28 01:14:53 +0600

    ACP

---
+++ ACP.py
@@ -0,0 +1,8 @@
+print("\033c")
+
+print("Hello World! Enter a number and see the square root operation!⬇\n")
+num = int(input("Enter a a Number: "))
+
+sqrt = num**(1/2)
+
+print(f"Square root of {num}↙️ \n🟰  {sqrt}")
